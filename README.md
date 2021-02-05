e5528108b275954a698122d14ac0cc95ccbc2010
hoal <hoal@umich.edu>
2021-02-04 19:49:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Bruh bot
+Replies bruh in the bruh channel and deletes all messages not bruh<br />
+Create a .env file and set your token equal to TOKEN <br />
---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+import os
+import string
+
+# Opens the client
+client = discord.Client()
+
+@client.event
+# If there's a message sent
+async def on_message(message):
+  # make sure it's not the bot
+  if message.author == client.user:
+    return
+
+  # if it's in the bruh channel
+  if message.channel.name == "bruh":
+    # check to make sure it only contains the words bruh
+    # this is not working, so I'm doing another trick
+    #lst = re.split(string.punctuation, message.content)
+    lst = "".join([" " if c in {i for i in string.punctuation} else c for c in message.content]).split()
+
+    # Checks to make sure every word is bruh
+    for word in lst:
+      if word.lower() != "bruh":
+        await message.delete()
+        return
+    await message.channel.send("Bruh")
+
+client.run(os.getenv("TOKEN"))
